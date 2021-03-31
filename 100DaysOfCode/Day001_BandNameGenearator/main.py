621f1dd15d8b8407195ff1287d4b1594aa3fdc4e
blackdogcode <blackdogcode@gmail.com>
2021-03-31 13:36:32 +0900

    Day1 Band Name Generator

---
+++ 100DaysOfCode/Day001_BandNameGenearator/main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    city_name = input('Type name of city you grew up in\n--> ')
+    pet_name = input('Type name of your pet\n--> ')
+
+    print(f'Recommended your band name\n--> {city_name} {pet_name}')
