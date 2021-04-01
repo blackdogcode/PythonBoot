531d64f9f347f2989f2b82eafa246bc7c82a98b6
blackdogcode <blackdogcode@gmail.com>
2021-04-01 13:34:07 +0900

    Tip Calculator

---
+++ 100DaysOfCode/Day002_TipCalculator/main.py
@@ -0,0 +1,12 @@
+# Python Rounding Reference: https://realpython.com/python-rounding/
+
+if __name__ == '__main__':
+    print(f'Welcome to the tip calculator')
+
+    total_bill = float(input(f'What was the total bill($)?: '))
+    tip_percentage = float(input(f'What percentage tip would you like to give(%)? 10, 12, 15? '))
+    num_of_people = float(input(f'How many people to split the bill?: '))
+
+    bill = total_bill / num_of_people
+    tip = total_bill * tip_percentage / 100 / num_of_people
+    print(f'Each person should pay: ${round(bill+tip, 2)}')
