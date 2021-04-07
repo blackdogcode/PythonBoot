3341bfe0276bff4c370f1e4e2449cea8eaaa7de4
blackdogcode <blackdogcode@gmail.com>
2021-04-07 16:13:55 +0900

    Reeborg Hurdl2 Solution

---
+++ 100DaysOfCode/Day006/Hurdle2_Reeborg.py
@@ -0,0 +1,17 @@
+def turn_right():
+    for i in range(3): turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    jump()
