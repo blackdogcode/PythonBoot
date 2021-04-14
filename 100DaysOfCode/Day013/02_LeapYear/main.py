36ce7fbba9d9e5754258f08e6f4384a9ef649c59
blackdogcode <blackdogcode@gmail.com>
2021-04-14 12:39:38 +0900

    debugging exercise with checking leap year

---
+++ 100DaysOfCode/Day013/02_LeapYear/main.py
@@ -0,0 +1,15 @@
+# 2. debugging exercise
+
+if __name__ == "__main__":
+    # year = input("Which year do you want to check?: ")
+    year = int(input("Which year do you want to check?: "))
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year.")
+            else:
+                print("Not leap year.")
+        else:
+            print("Leap year.")
+    else:
+        print("Not leap year.")
