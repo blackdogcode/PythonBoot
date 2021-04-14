a08e98aab8900c2417ac42da344debfc71762d51
blackdogcode <blackdogcode@gmail.com>
2021-04-14 12:44:57 +0900

    fix if-else statements bug

--- 100DaysOfCode/Day013/03_FizzBuzz/main.py
+++ 100DaysOfCode/Day013/03_FizzBuzz/main.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
         # if number % 3 == 0 or number % 5 == 0:
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz")
-        if number % 3 == 0:
+        # if number % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz")
-        if number % 5 == 0:
+        # if number % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz")
         else:
             # print([number])
