3b303812579ee61a05e630c8a888b67a31e00455
Liam, Song <blackdogcode@gmail.com>
2021-09-29 09:54:20 +0900

    Kth number problem in Programmmers

---
+++ Algorithm/KthNumber/main.py
@@ -0,0 +1,8 @@
+# https://t.ly/QdBI
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i-1:j])[k-1])
+    return answer
