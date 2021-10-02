6fb191a34fdfffe4409be050420f756427974d9e
Liam, Song <blackdogcode@gmail.com>
2021-10-02 20:19:12 +0900

    시간복잡도 개선 필요

--- Algorithm/ALLERGY/main.py
+++ Algorithm/ALLERGY/main.py
@@ -1,8 +1,6 @@
-def show_food_table(food_table):
-    for row in food_table:
-        print(row)
-
-
+# https://algospot.com/judge/problem/read/ALLERGY
+# 재귀 호출 로는 2^50 이라는 시간 복잡도내에 수행할 수 없다
+# 알고리즘 수행 속도를 개선하는 방법을 고안이 필요하다
 def can_everyone_eat(menu, food_table, n, m):
     ret = [0] * n
     for food in menu:
@@ -32,22 +30,20 @@ def min_select_menu(menu, food, food_table, n, m):
     return ret
 
 
-with open('test.txt', 'r') as input_file:
-    test_case = int(input_file.readline())
-    for _ in range(test_case):
-        n, m = map(int, input_file.readline().split())
-        # 0: 먹을 수 없음, 1: 먹을 수 있음
-        food_table = [[0 for _ in range(m)] for _ in range(n)]
+test_case = int(input())
+for _ in range(test_case):
+    n, m = map(int, input().split())
+    # 0: 먹을 수 없음, 1: 먹을 수 있음
+    food_table = [[0 for _ in range(m)] for _ in range(n)]
 
-        friends = input_file.readline().split()
+    friends = input().split()
 
-        # 각 친구 들이 먹을 수 있는 음식 정보 업데이트
-        for food_idx in range(m):
-            eat_info = input_file.readline().split()[1:]
-            for friend in eat_info:
-                friend_idx = friends.index(friend)
-                food_table[friend_idx][food_idx] = 1
+    # 각 친구 들이 먹을 수 있는 음식 정보 업데이트
+    for food_idx in range(m):
+        eat_info = input().split()[1:]
+        for friend in eat_info:
+            friend_idx = friends.index(friend)
+            food_table[friend_idx][food_idx] = 1
 
-        show_food_table(food_table)
-        menu = []
-        print(min_select_menu(menu, 0, food_table, n, m))
+    menu = []
+    print(min_select_menu(menu, 0, food_table, n, m))
