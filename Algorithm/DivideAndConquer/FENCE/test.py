2a45151d2772174f6cb1c0d8932c61c977db0baa
Liam, Song <blackdogcode@gmail.com>
2021-10-08 14:49:14 +0900

    분할정복을 이용한 최대직사각형 넓이 구하기

---
+++ Algorithm/DivideAndConquer/FENCE/test.py
@@ -0,0 +1,35 @@
+def solve(left, right, fence):
+    # 기저사례: 판자가 하나밖에 없는 경우
+    if left == right:
+        return fence[left]
+
+    # 케이스 1, 2: 두 개로 나눈 각 구간 중 최대 넓이 직사각형이 있는 경우
+    # 분할: 두 구간으로 분할하여 최대값을 구한다
+    mid = (left + right) // 2
+    ret = max(solve(left, mid, fence), solve(mid + 1, right, fence))
+
+    # 케이스 3: 최대 넓이 직사각형이 두 구간에 걸친 경우
+    lo, hi = mid, mid + 1
+    height = min(fence[lo], fence[hi])
+    ret = max(ret, height * 2)  # 두 구간에 하나씩 걸치 사각형
+
+    while left < lo or hi < right:
+        # 항상 높이 높은 쪽으로 직사각형을 확장한다
+        if (hi < right) and (lo == left or fence[lo - 1] < fence[hi + 1]):
+            hi += 1
+            height = min(height, fence[hi])
+        else:
+            lo -= 1
+            height = min(height, fence[lo])
+        ret = max(ret, height * (hi - lo + 1))
+
+    return ret
+
+
+with open('test_input.txt') as input_file:
+    N = int(input_file.readline())
+    fence = []
+    for _ in range(N):
+        fence.append(int(input_file.readline()))
+    print(solve(0, len(fence) - 1, fence))
+
