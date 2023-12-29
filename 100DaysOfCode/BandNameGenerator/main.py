dbb98b5e1a91b4045534a411f67798a910480b29
Liam, Song <blackdogcode@gmail.com>
2023-12-29 12:10:33 +0900

    1 day of python course project

---
+++ 100DaysOfCode/BandNameGenerator/main.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator!')
+
+city_born = input("What's name of the city you grew up in?\n")
+
+pet_name = input("What is your pet's name?\n")
+
+print('Your band name could be ' + city_born + ' ' + pet_name)
