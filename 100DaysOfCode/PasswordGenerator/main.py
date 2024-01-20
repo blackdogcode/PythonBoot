415e7c480c95278fda06ea113a6686b18665a0c7
Liam, Song <blackdogcode@gmail.com>
2024-01-20 18:04:41 +0900

    Day 5 Project

---
+++ 100DaysOfCode/PasswordGenerator/main.py
@@ -0,0 +1,24 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = list()
+
+for _ in range(nr_letters):
+    password.append(random.choice(letters))
+
+for _ in range(nr_symbols):
+    password.append(random.choice(symbols))
+
+for _ in range(nr_numbers):
+    password.append(random.choice(numbers))
+
+random.shuffle(password)
+
+print(f"Your password is: {''.join(password)}")
