80b162633dfe93b769e2e9f289a37ffd964afb06
Liam, Song <blackdogcode@gmail.com>
2024-01-21 16:51:05 +0900

    Day 6 Project

---
+++ 100DaysOfCode/EscapingMaze/main.py
@@ -0,0 +1,18 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     for _ in range(3):
+#         turn_left()
+#
+# while not at_goal():
+#     if wall_on_right():
+#         if not front_is_clear():
+#             while not front_is_clear():
+#                 turn_left()
+#     else:
+#         if front_is_clear():
+#             turn_right()
+#         else:
+#             while not front_is_clear():
+#                 turn_right()
+#     move()
